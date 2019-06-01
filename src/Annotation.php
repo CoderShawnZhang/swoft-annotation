3c32398c237e2ef495640f7a581fb0f584eb27af
zhanghongbo <taobao1875@163.com>
2019-06-01 16:49:21 +0800

    init

---
+++ src/Annotation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghongbo
+ * Date: 2019/6/1
+ * Time: 下午4:47
+ */
+namespace SwoftRewrite\Annotation;
+
+class Annotation
+{
+    public function test()
+    {
+        echo "run Annotation";
+    }
+}
